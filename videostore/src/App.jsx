4617920a5ab912a1bddef7f2353219c97b5b610b
minshallw12 <111906475+minshallw12@users.noreply.github.com>
2023-03-27 13:55:59 -0600

    initial commit

---
+++ videostore/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import HomePage from './components/HomePage'
+
+export default function App() {
+
+  return  (
+    
+    <div className="app_container">
+      <h1>Video Store</h1>
+      <HomePage />
+    </div>
+
+  )
+
+}
---
+++ videostore/src/components/HomePage.jsx
@@ -0,0 +1,5 @@
+export default function HomePage(props) {
+
+    return <h1>homepage running</h1>
+
+}
